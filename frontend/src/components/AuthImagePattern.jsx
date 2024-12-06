0df5a765648d5716fd94a482792b32f073a4a1a6
valeennmendez <112477437+valeennmendez@users.noreply.github.com>
2024-12-06 17:55:56 -0300

    Update AuthImagePattern.jsx

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -3,7 +3,7 @@ function AuthImagePattern({ title, subtitle }) {
     <div className="hidden lg:flex items-center justify-center bg-base-200 p12">
       <div className="max-w-md text-center rounded-lg ">
         <img
-          src="/public/image.png"
+          src="/image.png"
           alt=""
           className="transition duration-700 ease-in-out hover:opacity-70"
         />
