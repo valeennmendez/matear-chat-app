1555d9648361bf4f560f241c47a69cab9e322937
valeennmendez <112477437+valeennmendez@users.noreply.github.com>
2024-12-06 17:36:21 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,7 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 
   app.get("*", (req, res) => {
